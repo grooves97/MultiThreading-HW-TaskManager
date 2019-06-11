0e043d97ed611d40cded50acadce02b1a75835b6
Aseka <grooves97@gmail.com>
2019-06-11 22:05:20 +0600

    Программа завершает процесс

---
+++ TaskManager/TaskManager/TasksProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TaskManager
+{
+    public class TasksProcess
+    { 
+        public string ProcessName { get; set; }
+        public List<Process> GetProcessesByName()
+        {
+            return Process.GetProcesses().ToList();
+        }
+    }
+}
